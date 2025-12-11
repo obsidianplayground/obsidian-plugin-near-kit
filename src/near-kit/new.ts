10ed3548d62f60710b0e7b20fe43cafd3597aadb
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-12-10 18:37:31 -0600

    Update new.ts

--- src/near-kit/new.ts
+++ src/near-kit/new.ts
@@ -7,11 +7,11 @@ export const ACCOUNT_ID_STORAGE_KEY_CONST = "account_id";
 export const PRIVATE_KEY_STORAGE_KEY_CONST = "private_key";
 //
 // MY_BACKEND_ENV
-export const MY_BACKEND_NEAR_networkId: "mainnet" | "testnet" | "localnet" | "betanet" | { rpcUrl: string; networkId: string; } = (process.env.env_networkId as "mainnet" | "testnet" | "localnet" | "betanet") || "testnet";
-export const MY_BACKEND_NEAR_accountId = localStorage.getItem(ACCOUNT_ID_STORAGE_KEY_CONST);
+export const MY_BACKEND_NEAR_networkId: "mainnet" | "testnet" | "localnet" | "betanet" | { rpcUrl: string; networkId: string; } = (localStorage.getItem(NETWORK_STORAGE_KEY_CONST) as "mainnet" | "testnet" | "localnet" | "betanet") || "testnet";
+export const MY_BACKEND_NEAR_accountId: string | undefined = localStorage.getItem(ACCOUNT_ID_STORAGE_KEY_CONST) || undefined;
 //
 // Handle private key format (with or without ed25519: prefix)
-export const MY_BACKEND_NEAR_privateKey: PrivateKey | undefined = process.env.env_privateKey as PrivateKey | undefined;
+export const MY_BACKEND_NEAR_privateKey: PrivateKey | undefined = localStorage.getItem(PRIVATE_KEY_STORAGE_KEY_CONST) as PrivateKey | undefined;
 //
 // Initialize for backend/scripts
 export const near = new Near({
